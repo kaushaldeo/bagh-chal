4c9498b65c0b78df231e4b669e14953cbc4834b6
mirko.indlekofer@gmail.com <mirko.indlekofer@gmail.com@1c7faf7d-23a2-a18f-3736-8b821ac35e3c>
2011-04-08 09:17:33 +0000

    avatarWidget.h geaddet.

---
+++ trunk/BaghChal/BaghChal/avatarWidget.h
@@ -0,0 +1,18 @@
+#ifndef AVATARWIDGET_H
+#define AVATARWIDGET_H
+
+#include <QWidget>
+
+class AvatarWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit AvatarWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // AVATARWIDGET_H
