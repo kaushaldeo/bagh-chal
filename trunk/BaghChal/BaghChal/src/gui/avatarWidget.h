0566f0a6589ebec0045fb45b19bd9c28b0127ed7
sischnee@gmail.com <sischnee@gmail.com@1c7faf7d-23a2-a18f-3736-8b821ac35e3c>
2011-04-09 01:47:38 +0000

    Implementing Drag and Drop, added a dynamic property of type boolean called goat to the avatars

--- trunk/BaghChal/BaghChal/src/gui/avatarWidget.h
+++ trunk/BaghChal/BaghChal/src/gui/avatarWidget.h
@@ -3,12 +3,18 @@
 
 #include <QWidget>
 
+class QMouseEvent;
+
+
 class AvatarWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit AvatarWidget(QWidget *parent = 0);
 
+protected:
+	void mousePressEvent(QMouseEvent *event);
+    
 signals:
 
 public slots:
