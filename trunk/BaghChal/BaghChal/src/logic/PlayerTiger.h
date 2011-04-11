003cb5691c3c5c24a79bdc8dea6a6954ce4916a2
julian.bayer@onlinehome.de <julian.bayer@onlinehome.de@1c7faf7d-23a2-a18f-3736-8b821ac35e3c>
2011-04-11 18:13:32 +0000

    All logic headers added

---
+++ trunk/BaghChal/BaghChal/src/logic/PlayerTiger.h
@@ -0,0 +1,59 @@
+#ifndef PLAYERTIGER_H
+#define PLAYERTIGER_H 
+
+#include "Tiger.h"
+#include "Cell.h"
+
+/*! \file PlayerTiger.h
+ *  \brief Enthält Klasse die den Spieler repräsentiert, der die Tiger steuert.
+ */
+
+//! Klasse die den Spieler repräsentiert, der die Tiger steuert.
+class PlayerTiger
+{
+public:
+	/*! \brief Konstruktor.
+	 *
+	 *  Erzeugt 4 neue Tiger und setzt sie auf die übergebenen Eckfelder.
+	 *  Initialisiert score auf 0.
+	 *
+	 *  \param tigerCells Array mit Zeigern auf die 4 Eckfelder.
+	 */
+	PlayerTiger(Cell *tigerCells[]);
+
+	/*! \brief Destruktor.
+	 *
+	 *  Löscht Tiger.
+	 */
+	~PlayerTiger();
+
+	/*! \brief Überprüft, ob noch Züge möglich sind.
+	 * 
+	 *  Ruft Tiger::canMove() aller 4 Tiger auf.
+	 *  
+	 *  \return True, wenn noch Züge möglich sind, sonst false.
+	 */
+	bool canMove();
+
+	/*! \brief Ruft Tiger::move() auf.
+	 *
+	 *  Ermittelt betreffenden Tiger durch Vergleich des übergebenen Cellpointer
+	 *  mit den 4 Tigern und ruft die Tiger::move()-Funktion auf.
+	 *
+	 *  \param src Die Zelle, auf der der Tiger sich befindet.
+	 *  \param dst Die Zielzelle.
+	 *  \exception CanNotMoveException Wird geworfen, wenn der Zug ungültig ist.
+	 *  \return 1, wenn eine Ziege gefressen wurde, 0 sonst.
+	 */
+	int move(Cell *src, Cell *dst);
+
+private:
+	//! Array, das Zeiger auf die 4 Tiger enthält.
+	Tiger *tigers[4];
+
+	//! Anzahl der gefressenen Ziegen.
+	int score;
+
+};
+
+#endif
