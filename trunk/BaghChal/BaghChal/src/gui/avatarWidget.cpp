00541296d3b074181a267f4039adf8e8d6f1ad48
mirko.indlekofer@gmail.com <mirko.indlekofer@gmail.com@1c7faf7d-23a2-a18f-3736-8b821ac35e3c>
2011-06-03 16:49:12 +0000

    Verschwinden von Figuren gelöst.

--- trunk/BaghChal/BaghChal/src/gui/avatarWidget.cpp
+++ trunk/BaghChal/BaghChal/src/gui/avatarWidget.cpp
@@ -87,7 +87,8 @@ void AvatarWidget::mousePressEvent(QMouseEvent *event)
     drag->setHotSpot(event->pos() - *p);
     child->hide();
     
-    if (drag->exec(Qt::MoveAction) == Qt::MoveAction)
+    //if (drag->exec(Qt::MoveAction) == Qt::MoveAction)
+    if (drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction) == Qt::MoveAction)
     {
         //do nothing yet, could be used for special actions if dropping is successful
     }
