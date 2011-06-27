8ce27bf09220cfce58ddd987baf5936ff14d7375
mirko.indlekofer <mirko.indlekofer@gmail.com>
2011-06-27 22:35:40 +0000

    doxygen erneut verbessert.

--- BaghChal/BaghChal/src/logic/PlayerGoat.cpp
+++ BaghChal/BaghChal/src/logic/PlayerGoat.cpp
@@ -7,11 +7,6 @@
 #include "Game.h"
 #include "PlayerGoat.h"
 
-/**
- * @brief Default constructor for PlayerGoat
- *
- * Creates 20 instances of Goat and stores them in an array.
- */
 PlayerGoat::PlayerGoat()
 {
     goats = new Goat* [20];
@@ -24,13 +19,6 @@ PlayerGoat::PlayerGoat()
     nextGoat = 0;
 }
 
-/**
- * @brief Overloaded constructor for PlayerGoat
- * @param playerTiger - A pointer to the opponent Player
- *
- * Creates 20 instances of Goat and stores them in an array and
- * registers playerTiger as the opponent.
- */
 PlayerGoat::PlayerGoat(PlayerTiger *playerTiger)
 {
     opponent = playerTiger;
@@ -45,11 +33,6 @@ PlayerGoat::PlayerGoat(PlayerTiger *playerTiger)
     nextGoat = 0;
 }
 
-/**
- * @brief Default destructor for PlayerGoat
- *
- * Deletes all instances of Goat and the array containing them.
- */
 PlayerGoat::~PlayerGoat()
 {
     for (int i = 0; i < 20; i++)
@@ -60,10 +43,6 @@ PlayerGoat::~PlayerGoat()
     delete[]  goats;
 }
 
-/**
- * @brief Set the next goat that has not been placed yet on cell
- * @param cell - Cell to set the Goat to
- */
 void PlayerGoat::setCell(Cell *cell)
 {
     goats[nextGoat]->setCell(cell);
@@ -73,10 +52,6 @@ void PlayerGoat::setCell(Cell *cell)
     nextGoat++;
 }
 
-/**
- * @brief Checks if any goat can make any valid moves
- * @return True if any goat can make any valid moves, else false
- */
 bool PlayerGoat::canMove()
 {
     if (nextGoat < 20)
@@ -95,14 +70,6 @@ bool PlayerGoat::canMove()
     return false;
 }
 
-/**
- * @brief Checks if any goat can be moved from src to dst
- * @param src - The Cell the Goat should reside on
- * @param dst - The Cell to be checked if the goat residing on src could be moved to
- * @return True if a valid move is possble, else false
- *
- * If src is NULL, canMoveThere() checks if an unplaced goat could be placed on dst.
- */
 bool PlayerGoat::canMoveThere(Cell *src, Cell *dst)
 {
     Goat *thisGoat;
@@ -136,14 +103,6 @@ bool PlayerGoat::canMoveThere(Cell *src, Cell *dst)
     return  thisGoat->canMoveThere(dst);
 }
 
-/**
- * @brief Moves a goat residing on src to dst if possible
- * @param src - The Cell which the goat should reside on
- * @param dst - The Cell which the goat should be moved to
- * @exception CanNotMoveException is thrown when a move from src to dst is not possible
- *
- * If src is NULL, an unplaced goat will be placed on dst if possible.
- */
 void PlayerGoat::move(Cell *src, Cell *dst)
 {
     if (!canMoveThere(src, dst))
@@ -187,55 +146,31 @@ void PlayerGoat::move(Cell *src, Cell *dst)
     }
 }
 
-/**
- * @brief Setter for the array of goats controlled by PlayerGoat
- * @param goats - Array of pointers to instances of Goat
- */
 void PlayerGoat::setGoats(Goat **goats)
 {
     this->goats = goats;
 }
 
-/**
- * @brief Getter for the array of goats controlled by PlayerGoat
- * @return Array of pointers to instances of Goat
- */
 Goat **PlayerGoat::getGoats()
 {
     return goats;
 }
 
-/**
- * @brief Setter for the index in the array of the goats to be placed next
- * @param number - Index of the goat to be placed next
- */
 void PlayerGoat::setNextGoat(int number)
 {
     nextGoat = number;
 }
 
-/**
- * @brief Getter for the index in the array of the goats to be placed next
- * @return Index of the goat to be placed next
- */
 int PlayerGoat::getNextGoat()
 {
     return nextGoat;
 }
 
-/**
- * @brief Setter for the pointer to the opponent player
- * @param playerTiger - Pointer to the opponent player
- */
 void PlayerGoat::setPlayerTiger(PlayerTiger *playerTiger)
 {
     opponent = playerTiger;
 }
 
-/**
- * @brief Getter for the pointer to the opponent player
- * @return playerTiger - Pointer to the opponent player
- */
 PlayerTiger *PlayerGoat::getPlayerTiger()
 {
     return opponent;
--- BaghChal/BaghChal/src/logic/PlayerGoat.h
+++ BaghChal/BaghChal/src/logic/PlayerGoat.h
@@ -1,3 +1,8 @@
+/** @file PlayerGoat.h
+ *  @brief Headerfile for PlayerGoat class
+ *  @author Steffen Dittmar
+ */
+ 
 #ifndef PLAYERGOAT_H
 #define PLAYERGOAT_H
 
@@ -5,11 +10,6 @@
 #include "Cell.h"
 #include "PlayerTiger.h"
 #include "Exceptions.h"
-
-/** @file PlayerGoat.h
- *  @brief Headerfile for PlayerGoat class
- *  @author Steffen Dittmar
- */
 
 class PlayerTiger;
 
@@ -19,18 +19,95 @@ class PlayerTiger;
 class PlayerGoat
 {
 public:
+    /**
+     * @brief Default constructor for PlayerGoat
+     *
+     * Creates 20 instances of Goat and stores them in an array.
+     */
     PlayerGoat();
+
+    /**
+     * @brief Overloaded constructor for PlayerGoat
+     * @param playerTiger - A pointer to the opponent Player
+     *
+     * Creates 20 instances of Goat and stores them in an array and
+     * registers playerTiger as the opponent.
+     */
     PlayerGoat(PlayerTiger *playerTiger);
+
+    /**
+     * @brief Default destructor for PlayerGoat
+     *
+     * Deletes all instances of Goat and the array containing them.
+     */
     ~PlayerGoat();
+
+    /**
+     * @brief Set the next goat that has not been placed yet on cell
+     * @param cell - Cell to set the Goat to
+     */
     void setCell(Cell *cell);
+
+    /**
+     * @brief Checks if any goat can make any valid moves
+     * @return True if any goat can make any valid moves, else false
+     */
     bool canMove();
+
+    /**
+     * @brief Checks if any goat can be moved from src to dst
+     * @param src - The Cell the Goat should reside on
+     * @param dst - The Cell to be checked if the goat residing on src could be moved to
+     * @return True if a valid move is possble, else false
+     *
+     * If src is NULL, canMoveThere() checks if an unplaced goat could be placed on dst.
+     */
     bool canMoveThere(Cell *src, Cell *dst);
+
+    /**
+     * @brief Moves a goat residing on src to dst if possible
+     * @param src - The Cell which the goat should reside on
+     * @param dst - The Cell which the goat should be moved to
+     * @exception CanNotMoveException is thrown when a move from src to dst is not possible
+     *
+     * If src is NULL, an unplaced goat will be placed on dst if possible.
+     */
     void move(Cell *src, Cell *dst);
+
+    /**
+     * @brief Setter for the array of goats controlled by PlayerGoat
+     * @param goats - Array of pointers to instances of Goat
+     */
     void setGoats(Goat **goats);
+
+    /**
+     * @brief Getter for the array of goats controlled by PlayerGoat
+     * @return Array of pointers to instances of Goat
+     */
     Goat **getGoats();
+
+    /**
+     * @brief Setter for the index in the array of the goats to be placed next
+     * @param number - Index of the goat to be placed next
+     */
     void setNextGoat(int number);
+
+    /**
+     * @brief Getter for the index in the array of the goats to be placed next
+     * @return Index of the goat to be placed next
+     */
     int getNextGoat();
+
+    /**
+     * @brief Setter for the pointer to the opponent player
+     * @param playerTiger - Pointer to the opponent player
+     */
     void setPlayerTiger(PlayerTiger *playerTiger);
+
+    /**
+     * @brief Getter for the pointer to the opponent player
+     * @return playerTiger - Pointer to the opponent player
+     */
     PlayerTiger *getPlayerTiger();
 
 private:
