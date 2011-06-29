338c79006548a1680bf303222a06ceb96f77c16e
bubgarpehehenis@googlemail.com <bubgarpehehenis@googlemail.com@1c7faf7d-23a2-a18f-3736-8b821ac35e3c>
2011-06-29 16:28:07 +0000

    Added inline comments for PlayerTiger, PlayerGoat, Tiger and Goat.

--- trunk/src/logic/Tiger.cpp
+++ trunk/src/logic/Tiger.cpp
@@ -18,6 +18,7 @@ Tiger::Tiger(Cell *cell)
 
 bool Tiger::canMove()
 {
+	//For every possible direction
     for (int i = 0; i < 8; i++)
     {
         Cell *temp;
@@ -28,15 +29,18 @@ bool Tiger::canMove()
         }
         catch (CanNotMoveException e)
         {
+			//Tiger can not move in direction i, proceed to the next direction
             continue;
         }
 
         if (temp->getStatus() == empty)
         {
+			//Tiger can move in at least one direction
             return true;
         }
         else if (temp->getStatus() == goat)
         {
+			//Tiger could possibly eat a goat
             Cell *nextTemp;
 
             try
@@ -45,16 +49,19 @@ bool Tiger::canMove()
             }
             catch (CanNotMoveException e)
             {
+				//Tiger can not jump over the goat in direction i, proceed to next direction
                 continue;
             }
 
             if (nextTemp->getStatus() == empty)
             {
+				//Tiger can move in at least one direction
                 return true;
             }
         }
     }
 
+	//Tiger can't move in any direction
     return false;
 }
 
@@ -68,31 +75,41 @@ bool Tiger::canMoveThere(Cell *cell)
 
         if (cell->getStatus() == empty)
         {
+			//Cell is next to cell the tiger resides on and is empty
             return true;
         }
 
+		//Cell is next to the cell the tiger resides on, but is not empty
         return false;
     }
     catch (InvalidDirectionException e)
     {
+		//Cell is not next to the cell the tiger resides on
         try
         {
             dir = cellPtr->isJumpOverNeighbor(cell);
 
             if (cell->getStatus() == empty)
             {
+				//The cell next to the one this tiger resides on is occupied
+				//by a goat and the cell next to it in the same direction is
+				//empty
                 return true;
             }
 
+			//The cell next to the one this tiger resides on is occupied
+			//by a goat, but the one next to it in the same direction is
+			//not empty
             return false;
         }
         catch (InvalidDirectionException e)
         {
+			//Cell is not next to a cell next to the cell the tiger resides on
             return false;
         }
     }
 
-
+	//Paranoia
     return false;
 }
 
@@ -111,6 +128,7 @@ int Tiger::move(Cell *cell, bool mustEat)
 
         if (cell->getStatus() == empty)
         {
+			//Tiger is moved to an empty cell next to it's origin
             if (mustEat)
             {
                 throw new MustEatException();
@@ -128,6 +146,7 @@ int Tiger::move(Cell *cell, bool mustEat)
     {
         try
         {
+			//Tiger is about to eat a goat
             dir = cellPtr->isJumpOverNeighbor(cell);
             Cell *jumpOverCell = cellPtr->getNeighbor(dir);
             cellPtr->removeTiger();
@@ -141,12 +160,15 @@ int Tiger::move(Cell *cell, bool mustEat)
         }
         catch (InvalidDirectionException e)
         {
+			//Dst is not next to this tigers current cell and not
+			//next to the one next to it
             throw new CanNotMoveException();
         }
 
 
     }
 
+	//Paranoia
     throw new CanNotMoveException();
 }
 
@@ -164,18 +186,23 @@ bool Tiger::couldEat()
 {
     Direction allDirections[8] = {baghchal::right, baghchal::below, baghchal::left, baghchal::above, baghchal::rightAbove, baghchal::rightBelow, baghchal::leftBelow, baghchal::leftAbove};
 
+	//For every direction
     for (int i = 0; i < 8; ++i)
     {
         Cell *test;
 
         try
         {
+			//Check if the cell next to the one this tiger currently
+			//resides on contains a goat
             test = cellPtr->getNeighbor(allDirections[i]);
 
             if (test->getStatus() == baghchal::goat)
             {
                 try
                 {
+					//Check if the cell next to the one containing the goat
+					//in the same direction is empty
                     test = test->getNeighbor(allDirections[i]);
 
                     if (test->getStatus() == baghchal::empty)
@@ -185,15 +212,18 @@ bool Tiger::couldEat()
                 }
                 catch (CanNotMoveException e)
                 {
+					//Goat can not be eaten, proceed to the next direction
                     continue;
                 }
             }
         }
         catch (CanNotMoveException e)
         {
+			//Tiger can not move in direction i, proceed to next direction
             continue;
         }
     }
 
+	//No goat could be eaten
     return false;
 }
